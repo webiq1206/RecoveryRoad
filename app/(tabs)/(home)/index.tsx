f2a0bdc68295a0aab6ad58fa3692ba1962129de8
Todd Brost <toddbrost@gmail.com>
2026-04-04 15:05:50 -0600

    Describe what you changed

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,26 +1,3 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
-import { useUser } from '../../../core/domains/useUser';
-import { useAppStore } from '../../../stores/useAppStore';
-import { HomeLoadingSkeleton } from '../../../components/LoadingSkeleton';
-import { getStrictRedirectTarget } from '../../../utils/legacyRoutes';
-
-export default function HomeScreenRedirect() {
-  const { profile, isLoading } = useUser();
-  const centralProfile = useAppStore((s) => s.userProfile);
-
-  if (isLoading) {
-    return <HomeLoadingSkeleton />;
-  }
-
-  if (!(centralProfile?.hasCompletedOnboarding ?? profile.hasCompletedOnboarding)) {
-    return <Redirect href={'/onboarding' as any} />;
-  }
-
-  const strictTarget = getStrictRedirectTarget('/(tabs)/(home)');
-  if (strictTarget) {
-    return <Redirect href={strictTarget as any} />;
-  }
-
-  return <Redirect href={'/(tabs)/(home)/today-hub' as any} />;
+export default function Page() {
+  return null;
 }
